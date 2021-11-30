dd0a0941a1c8bb83d1c6a626037568dc80539d98
Austin Edwards <austin.w.edwards@gmail.com>
2021-11-29 17:59:25 -0800

    removed PyQT dependencies

--- viewer_main.py
+++ viewer_main.py
@@ -1,146 +1,83 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QSlider
-from magicgui import magicgui
-from magicgui.widgets import ComboBox
-
-from random import randrange
-import sys
-import imageio
 import napari
-import exifread
+from magicgui import magicgui
 import pathlib
 import numpy as np
 import pandas as pd
+from random import randrange
 import aicspylibczi
 
-class App(QWidget):
 
-    #@magicgui(
-    #        auto_call=True, threshold={"widget_type": QSlider, "minimum":1, "maximum":10}
-    #        )
+LUTs = ['blue','cyan','gray','green','magenta','red','yellow']
+channel_names = ['DAPI','HLADR','CD8','CD163','CD4','XCR1','CD3','PDL1','EPCAM']
 
-    def __init__(self):
-        super().__init__()
-        
-        # Limits to the linear monochrome LUTs within napari
-        self.available_LUTs = ['blue', 'cyan', 'gray', 'green', 'magenta', 'red', 'yellow']
+@magicgui(
+        call_button = "Save thresholds",
+        marker={"choices": ['DAPI','CD8','CD4','CD163','XCR1','HLADR','CD3','PDL1','Tumor']},
+        Threshold={"widget_type": "FloatSlider",'max':20}
+        )
 
-        self.run_all()
+def threshold_widget(
+        Threshold_value:float,
+        Threshold=0.0,
+        marker='DAPI',
+        image_filename = pathlib.Path('/some/path.czi'),
+        cell_data_filename = pathlib.Path('/some/path.csv')
+        ):
 
-    #def adjust_threshold(channel: str, threshold: int=5):
-    #    print(threshold)
+    pass
 
-    # Prompt to select MIBItiff. No error handling.
-    def get_working_directory(self, message):
-        path_str = (QFileDialog.getOpenFileName(self, message, "/media/austin/DrosophilaMelanogaster/IPI/8plex/data"))
-        return pathlib.Path(str(path_str[0]))
 
-    def get_saving_directory(self, message):
-        path_str = (QFileDialog.getSaveFileName(self, message))
-        return pathlib.Path(str(path_str[0]))
+@threshold_widget.image_filename.changed.connect
+def load_new_image(value: str):
+    im_data = []
 
-    # Opens the tiff file
-    def open_tiff(self, file_name):
-        image = imageio.mimread(file_name, multifile=True)
-        return image
+    czifile = aicspylibczi.CziFile(value)
+    dims = czifile.get_dims_shape()
+    nchannels = dims[0]['C'][1]
 
-    # Gets the channels from the tags
-    def get_tags(self, file_name, num_images):
-        f = open(file_name, 'rb')
-        tags = exifread.process_file(f)
-        channel_list = [str(tags['Image PageName']), str(tags['Thumbnail PageName'])]
-        for n in range(2, num_images):
-            channel_list.append(str(tags['IFD ' + str(n) + ' PageName']))
-
-        return channel_list
-
-    # Determine how many channels there should be
-    def get_image_length(self, image):
-        num_images = len(image)
-        return num_images
-
-    # launches napari with the fle and channel names
-    def launch_napari(self, image, channel_names, LUTs):
-
-        self.viewer = napari.Viewer()
-
-        self.file_menu = self.viewer.window.file_menu
-        self.add_load_csv_menu()
-        self.threshold_value = 0 # initialize threshold
-        for c in range(len(channel_names)):
-            self.viewer.add_image(image[c], name=channel_names[c], visible=False)
-            self.viewer.layers[c].name = channel_names[c]
-            self.viewer.layers[c].colormap = LUTs[randrange(len(LUTs))]
-            self.viewer.layers[c].opacity = 1.0
-            self.viewer.layers[c].blending = 'additive'
-            self.viewer.layers[c].interpolation = 'gaussian'
-        
-        self.create_channel_menu(channel_names)
-        #self.slider = scrollThreshold.Gui()
-        #self.slider.threshold_widget.channel_changed.connect(update_slider)
-
-    #def update_slider(self,channel):
-    #    
-    #    self.slider.threshold_widget.setMaximum(np.max(self.cell_data['channel']))
-    #    self.slider.threshold_widget.setMinimum(np.min(self.cell_data['channel']))
-
-    def load_csv(self):
-        csvfilter = "Comma-Separated Value File (*.csv)"
-        path_str = QFileDialog.getOpenFileName(self, "Select data table for the current image", "/media/austin/DrosophilaMelanogaster/IPI/8plex/data", filter=csvfilter)
-        self.cell_data = pd.read_csv(pathlib.Path(str(path_str[0])))
-
-        #self.x = np.array(self.cell_data['centroid-0'])
-        #self.y = np.array(self.cell_data['centroid-1'])
-        #self.points = np.stack((self.x,self.y)).transpose()
-        #self.points_layer = self.viewer.add_points(self.points, size = 20)
-
-    def add_load_csv_menu(self):
-        self.csv_menu = self.file_menu.addAction('Load cell data table...')
-        self.csv_menu.triggered.connect(self.load_csv)
+    for c in np.arange(nchannels):
+        print("Loading channel " + str(c))
+        im_data.append(czifile.read_mosaic(C=c,scale_factor=1))
     
-    def create_channel_menu(self, labels):
-        self.channel_menu = ComboBox(label='channel_threshold', choices=labels)
-        self.viewer.window.add_dock_widget(self.channel_menu)
-        
-        def label_changed(event):
-            
-            self.selected_channel = event.value
-            #self.update_slider()
-            self.update_scatter()
-        self.channel_menu.changed.connect(label_changed)
+    image = np.stack(im_data)
+
+    for c in range(len(channel_names)):
+
+        viewer.add_image(image[c], name=channel_names[c], visible=False)
+        viewer.layers[c].colormap = LUTs[randrange(len(LUTs))]
+        viewer.layers[c].opacity = 1.0
+        viewer.layers[c].blending = 'additive'
+        viewer.layers[c].interpolation = 'gaussian'
+   
+    threshold_dict = {}
+    for c in channel_names:
+        threshold_dict[c] = 0.0
+
+@threshold_widget.cell_data_filename.changed.connect
+def load_cell_data(value: str):
+
+    data = pd.read_csv(value)
+
+    x = np.array(data['centroid-0'])
+    y = np.array(data['centroid-1'])
+
+    points = np.stack((x, y)).transpose()
+
+    points_layer = viewer.add_points(points, size=25)
+
+@threshold_widget.Threshold.changed.connect
+def threshold_slider_change(value: float):
+
+    threshold_dict[threshold_widget.marker] = value
     
-    def update_scatter(self):
-       
-       result = self.cell_data[self.cell_data[self.selected_channel] > self.threshold_value]
-       self.x = np.array(result['centroid-0'])
-       self.y = np.array(result['centroid-1'])
-       self.points = np.stack((self.x, self.y)).transpose()
-       self.points_layer = self.viewer.add_points(self.points, size=20)
-        
-    # Runs the program
-    def run_all(self):
-        self.image_path = self.get_working_directory("Select .czi image")
-        
-        im_data = []
+    result = data[data[threshold_widget.marker] > value]
 
-        if str(self.image_path).endswith(".czi"):
-            img = aicspylibczi.CziFile(str(self.image_path))
-            dims = img.get_dims_shape()
-            nchannels = dims[0]['C'][1]
-            
-            for c in np.arange(nchannels):
-                print("Loading channel " + str(c))
-                im_data.append(img.read_mosaic(C=c,scale_factor=1))
-        
-            self.im = np.stack(im_data)
-        if self.image_path.name.startswith("IPICRC"):
-            self.channels = ['DAPI','HLADR','CD8','CD163','CD4','XCR1','CD3','PDL1','EPCAM']
-        else:
-            self.channels = ['DAPI','HLADR','CD8','CD163','CD4','XCR1','CD3','PDL1','PanCK']
-        self.launch_napari(self.im, self.channels, self.available_LUTs)
+    print(len(result))
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = App()
-    sys.exit(app.exec_())
+
+viewer = napari.Viewer()
+viewer.window.add_dock_widget(threshold_widget)
+   
+napari.run()
+
