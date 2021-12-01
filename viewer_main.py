67b46d7ac9641c509fc4e125b6394e7f0a542422
Austin Edwards <austin.w.edwards@gmail.com>
2021-12-01 14:52:20 -0800

    added histogram visualization for marker expression

--- viewer_main.py
+++ viewer_main.py
@@ -1,4 +1,5 @@
 import napari
+from napari_properties_plotter import PropertyPlotter as propplot
 from magicgui import magicgui
 import pathlib
 import numpy as np
@@ -6,19 +7,19 @@ import pandas as pd
 from random import randrange
 import aicspylibczi
 
-
+    
 LUTs = ['blue','cyan','gray','green','magenta','red','yellow']
-channel_names = ['DAPI','HLADR','CD8','CD163','CD4','XCR1','CD3','PDL1','EPCAM']
+channel_names = ['DAPI','HLADR','CD8','CD163','CD4','XCR1','CD3','PDL1','PanCK']
 
 @magicgui(
         call_button = "Save thresholds",
-        marker={"choices": ['DAPI','CD8','CD4','CD163','XCR1','HLADR','CD3','PDL1','Tumor']},
-        Threshold={"widget_type": "FloatSlider",'max':20}
+        marker={"choices": [('DAPI', 'DAPI'),('CD3','CD3'),('CD4','CD4'),('CD8','CD8'),('CD163','CD163'),('XCR1','XCR1'),('HLADR','HLADR'),('PDL1','PDL1'),('Tumor','PanCK')]},
+        threshold_slider={"widget_type": "FloatSlider",'max':20}
         )
 
 def threshold_widget(
         Threshold_value:float,
-        Threshold=0.0,
+        threshold_slider=0.0,
         marker='DAPI',
         image_filename = pathlib.Path('/some/path.czi'),
         cell_data_filename = pathlib.Path('/some/path.csv')
@@ -40,6 +41,11 @@ def load_new_image(value: str):
         im_data.append(czifile.read_mosaic(C=c,scale_factor=1))
     
     image = np.stack(im_data)
+    if "CRC" in threshold_widget.image_filename.value.name:
+        channel_names[-1] = 'EPCAM'
+        threshold_widget.marker.set_choice('Tumor','EPCAM')
+        print(threshold_widget.marker.choices)
+        print(threshold_widget.marker.get_choice('Tumor'))
 
     for c in range(len(channel_names)):
 
@@ -57,27 +63,31 @@ def load_new_image(value: str):
 def load_cell_data(value: str):
 
     data = pd.read_csv(value)
-
+    n_points = len(data)
     x = np.array(data['centroid-0'])
     y = np.array(data['centroid-1'])
 
     points = np.stack((x, y)).transpose()
-
-    points_layer = viewer.add_points(points, size=25)
-
-@threshold_widget.Threshold.changed.connect
-def threshold_slider_change(value: float):
-
-    threshold_dict[threshold_widget.marker] = value
     
-    result = data[data[threshold_widget.marker] > value]
+    points_layer = viewer.add_points(points,
+            size=25,
+            properties=data,
+            face_color=threshold_widget.marker.value,
+            name='points',
+            edge_color_cycle=['green', 'red'])
 
-    print(len(result))
+@threshold_widget.threshold_slider.changed.connect
+def threshold_slider_change(value: float):
+    tf = np.array(viewer.layers['points'].properties[threshold_widget.marker.value] > value, dtype=np.uint8)
+    viewer.layers['points'].properties[threshold_widget.marker.value + "_phenotype"] = tf 
+    viewer.layers['points'].edge_color = viewer.layers['points'].properties[threshold_widget.marker.value + "_phenotype"]
 
-
+@threshold_widget.marker.changed.connect
+def update_points_facecolor(value: str):
+    viewer.layers['points'].face_color = value
 
 viewer = napari.Viewer()
 viewer.window.add_dock_widget(threshold_widget)
-   
+pp = propplot(viewer)
+viewer.window.add_dock_widget(pp, area='bottom')
 napari.run()
-
