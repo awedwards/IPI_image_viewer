36afca37a07df4b72b242da550496d5de4b0c246
Austin Edwards <austin.w.edwards@gmail.com>
2021-12-21 14:56:30 -0800

    fixed threshold slide, switching image bugs

--- viewer_main.py
+++ viewer_main.py
@@ -9,21 +9,28 @@ import aicspylibczi
 from datetime import datetime as dt
     
 LUTs = ['blue','cyan','gray','green','magenta','red','yellow']
+# harcoded channel names because the CZIs only have the marke names
 channel_names = ['DAPI','HLADR','CD8','CD163','CD4','XCR1','CD3','PDL1','PanCK']
 
+# initialize dictionary to store thresholds for each channel
 threshold_dict = {}
 
 for c in channel_names:
     threshold_dict[c] = 0.0
 
 @magicgui(
+        # call_button connects to save method
         call_button = "Save thresholds",
+        # name, value pairs for the marker drop down menu. The Tumor marker value is changed based on which type of sample is loaded in
         marker={"choices": [('DAPI', 'DAPI'),('CD3','CD3'),('CD4','CD4'),('CD8','CD8'),('CD163','CD163'),('XCR1','XCR1'),('HLADR','HLADR'),('PDL1','PDL1'),('Tumor','PanCK')]},
+        # cell type dropdown list
         cell_type={"choices":['CD4 T cells','CD8 T cells','cdc1','mac','DoubleNeg T cells','other myeloid/B cells','tumor HLADR+','other']},
         threshold_slider={"widget_type": "FloatSlider",'max':20}
         )
 
 def threshold_widget(
+        # Initalize widget values
+
         threshold_value:float,
         threshold_slider=0.0,
         marker='DAPI',
@@ -38,19 +45,22 @@ def threshold_widget(
 @threshold_widget.image_filename.changed.connect
 def load_new_image(value: str):
 
-    # clear old image
-
     im_data = []
 
     czifile = aicspylibczi.CziFile(value)
     dims = czifile.get_dims_shape()
     nchannels = dims[0]['C'][1]
     contrast_limits = []
+    
     for c in np.arange(nchannels):
-        print("Loading channel " + str(c))
-        im_data.append(czifile.read_mosaic(C=c,scale_factor=1))
-        contrast_limits.append([0, 2**16])
-    image = np.stack(im_data)
+        
+        #clear old channel image
+        try:
+            viewer.layers.pop(channel_names[c])
+        except KeyError:
+            pass
+    
+    # Check the type of sample and adjust the tumor marker value. Will need to add MUC-1 and MelanA soon.
     if "CRC" in threshold_widget.image_filename.value.name:
         channel_names[-1] = 'EPCAM'
         threshold_widget.marker.set_choice('Tumor','EPCAM')
@@ -61,14 +71,13 @@ def load_new_image(value: str):
         threshold_widget.marker.set_choice('Tumor','PanCK')
 
     for c in range(len(channel_names)):
-        
-        #clear old channel image
-        try:
-            viewer.layers.pop(channel_names[c])
-        except KeyError:
-            pass
-
-        viewer.add_image(image[c], name=channel_names[c], visible=False, contrast_limits=contrast_limits[c])
+        #load in new channel
+        print("Loading channel " + str(c))
+        image = czifile.read_mosaic(C=c,scale_factor=1)
+        contrast_limits.append([0, 2**16])
+         
+        # add each channel
+        viewer.add_image(image, name=channel_names[c], visible=False, contrast_limits=contrast_limits[c])
         viewer.layers[c].colormap = LUTs[randrange(len(LUTs))]
         viewer.layers[c].opacity = 1.0
         viewer.layers[c].blending = 'additive'
@@ -88,9 +97,11 @@ def load_cell_data(value: str):
     x = np.array(data['centroid-0'])
     y = np.array(data['centroid-1'])
 
+    # Initalized column for threshold results
     for c in channel_names:
         data[c + "_expressed"] = np.array(data[c] > threshold_dict[c], dtype=np.int8)
     
+    # format data for adding as layer
     points = np.stack((x, y)).transpose()
     
     points_layer = viewer.add_points(points,
@@ -103,6 +114,9 @@ def load_cell_data(value: str):
 @threshold_widget.threshold_slider.changed.connect
 def threshold_slider_change(value: float):
     
+    threshold_widget.threshold_value.value = value
+    
+    # remove old threhsold results
     try:
         viewer.layers.pop('threshold result')
     except KeyError:
@@ -110,13 +124,17 @@ def threshold_slider_change(value: float):
 
     channel = threshold_widget.marker.value
     data = pd.DataFrame.from_dict(viewer.layers['points'].properties)
+    
+    x = np.array(data['centroid-0'])
+    y = np.array(data['centroid-1'])
+    all_points = np.stack((x,y)).transpose()
+
     thresholded_data = data[data[channel] > value]
     x = np.array(thresholded_data['centroid-0'])
     y = np.array(thresholded_data['centroid-1'])
 
     points = np.stack((x, y)).transpose()
     
-    threshold_widget.threshold_value.value = value
     threshold_dict[channel] = value
     
     data[channel + "_expressed"] = np.array(data[channel] > threshold_dict[channel], dtype=np.int8)
@@ -129,17 +147,12 @@ def threshold_slider_change(value: float):
                 name='threshold result',
                 visible=True)
     
-    x = np.array(data['centroid-0'])
-    y = np.array(data['centroid-1'])
-
-    points = np.stack((x,y)).transpose()
-    
     try:
         viewer.layers.pop('points')
     except KeyError:
         pass
 
-    points_layer = viewer.add_points(points,
+    points_layer = viewer.add_points(all_points,
             size=25,
             properties=data,
             face_color=threshold_widget.marker.value,
@@ -226,7 +239,9 @@ def update_cell_types():
 
     data['cell_type'][(data['DAPI_expressed']==1) & \
             (data['XCR1_expressed']==1) & \
-            (data['HLADR_expressed']==1)] = 'cdc1'
+            (data['HLADR_expressed']==1) & \
+            (data['CD3_expressed']==0) & \
+            (data['CD163_expressed']==0)] = 'cdc1'
 
     data['cell_type'][(data['DAPI_expressed']==1) & \
             (data['CD3_expressed']==1) & \
